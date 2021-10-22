308c1490ac292bdc77f79c948b767b00cdacc85f
Ala <ala8362@yahoo.com>
2021-10-22 05:54:58 -0700

    book store

---
+++ frontend1.py
@@ -0,0 +1,118 @@
+from tkinter import *
+import backend1
+window=Tk()
+window.title("BOOk STORE")
+
+def clear_list():
+    list1.delete(0,END)
+
+def fill_list(books):
+    for book in books:
+        list1.insert(END,book)
+
+
+#====================Labels==========================
+
+l1=Label(window,text="Title")
+l1.grid(row=0,column=0)
+l2=Label(window,text="Author")
+l2.grid(row=0,column=2)
+l3=Label(window,text="Year")
+l3.grid(row=1,column=0)
+l4=Label(window,text="ISBN")
+l4.grid(row=1,column=2)
+
+#===================Entries=========================
+
+title_text=StringVar()
+e1=Entry(window,textvariable=title_text)
+e1.grid(row=0,column=1)
+
+author_text=StringVar()
+e2=Entry(window,textvariable=author_text)
+e2.grid(row=0,column=3)
+
+year_text=StringVar()
+e3=Entry(window,textvariable=year_text)
+e3.grid(row=1,column=1)
+
+isbn_text=StringVar()
+e4=Entry(window,textvariable=isbn_text)
+e4.grid(row=1,column=3)
+
+list1=Listbox(window,width=35,height=6)
+list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+sb1=Scrollbar(window)
+sb1.grid(row=2,column=2,rowspan=6)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+def get_selected_row(event):
+    global selected_book
+    if len(list1.curselection())>0:
+        index=list1.curselection()[0]
+        selected_book=list1.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_book[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_book[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_book[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_book[4])
+    
+
+list1.bind("<<ListboxSelect>>",get_selected_row)
+
+#=========================Buttons==============================
+
+def view_command():
+    clear_list()
+    books=backend1.view()
+    fill_list(books)
+  
+
+btn1=Button(window,text="View All",width=12,command=lambda:view_command())
+btn1.grid(row=2,column=3)
+
+def search_command():
+    clear_list()
+    books=backend1.search(title=title_text.get(),author=author_text.get(),year=year_text.get(),isbn=isbn_text.get())
+    fill_list(books)
+
+
+btn2=Button(window,text="Search Entry",width=12, command=search_command)
+btn2.grid(row=3,column=3)
+
+def add_command():
+    backend1.insert(title=title_text.get(),author=author_text.get(),year=year_text.get(),isbn=isbn_text.get())
+    view_command()
+
+
+btn3=Button(window,text="Add Entry",width=12,command=add_command)
+btn3.grid(row=4,column=3)
+
+def update_command():
+    backend1.update(selected_book[0],title=title_text.get(),author=author_text.get(),year=year_text.get(),isbn=isbn_text.get())
+    view_command()
+
+btn4=Button(window,text="Update Selected",width=12, command=update_command)
+btn4.grid(row=5,column=3)
+
+def delete_command():
+    backend1.delete(selected_book[0])
+    view_command()
+
+
+
+btn5=Button(window,text="Delete Selected",width=12,command=delete_command)
+btn5.grid(row=6,column=3)
+
+btn6=Button(window,text="Close",width=12, command=window.destroy)
+btn6.grid(row=7,column=3)
+
+
+view_command()
+window.mainloop()
